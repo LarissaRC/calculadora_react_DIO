9ad6e36cd52666bedf9044cd0fb336d29410e015
LarissaRC <50839355+LarissaRC@users.noreply.github.com>
2023-09-07 23:20:46 -0400

    Soma e subtração

---
+++ desafio-calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.div`
+    padding: 10px;
+    border: 1px solid #CDCDCD;
+    background-color: #DD4400;
+    color: white;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
